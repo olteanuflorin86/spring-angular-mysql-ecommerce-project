e6e8e8036ffc43133e4edbd02a32cc40df683a90
olteanuflorin86 <olteanuflorin86@gmail.com>
2021-05-23 07:28:52 +0300

    Search for Products by Category

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
+
 
 @Component({
   selector: 'app-product-list',
@@ -12,15 +14,33 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
 
   products: Product[]; 
+  currentCategoryId: number;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, 
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    })
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+
+    // check if id parameter is available:
+    const hasCategoryId = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the id parameter and convert the string value to a number using "+" operator
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    } 
+    else {
+      // id not available, we'll set a default id to one:
+      this.currentCategoryId = 1;
+    }
+
+    // get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
