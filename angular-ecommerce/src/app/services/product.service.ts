8f195f1086c56b9bf125c4f21f23bbc92e4949d7
olteanuflorin86 <olteanuflorin86@gmail.com>
2021-05-24 09:32:00 +0300

    Product Master-Detail View

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -45,6 +45,14 @@ export class ProductService {
       map(response => response._embedded.products)
     );
   }
+
+  getProduct(theProductId: number): Observable<Product> {
+
+    // need to build URL based on product id
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+    
+    return this.httpClient.get<Product>(productUrl);
+  }
 }
 
 interface GetResponseProduct {
